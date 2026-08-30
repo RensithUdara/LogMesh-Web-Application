0633e545c0b320bbfdb74bb0b6a3819c005e9c70
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-08-30 19:16:50 +0530

    feat: define Project, User, AuthRequest, and AuthResponse types

---
+++ internal/model/project.go
@@ -0,0 +1,27 @@
+package model
+
+import "time"
+
+type Project struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type User struct {
+	ID           string    `json:"id"`
+	ProjectID    string    `json:"project_id"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+type AuthRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type AuthResponse struct {
+	Token string `json:"token"`
+	User  User   `json:"user"`
+}
