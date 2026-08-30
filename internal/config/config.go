1ea7a09dede58c0b351e59741bc59e3e7e5e4509
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-08-30 19:15:46 +0530

    feat: add additional configuration options for JWT, Kafka, OpenSearch, Postgres, Redis, and processor settings

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,6 +15,16 @@ type Config struct {
 	RequireAPIKey     bool
 	RateLimitRequests int
 	RateLimitWindow   int
+	JWTSecret         string
+	KafkaBrokers      string
+	KafkaLogsTopic    string
+	KafkaDLQTopic     string
+	OpenSearchURL     string
+	PostgresURL       string
+	RedisURL          string
+	ProcessorWorkers  int
+	ProcessorBatchSize int
+	ProcessorBatchMS  int
 }
 
 func Load() Config {
@@ -26,6 +36,16 @@ func Load() Config {
 		RequireAPIKey:     envBool("LOGMESH_REQUIRE_API_KEY", false),
 		RateLimitRequests: envInt("LOGMESH_RATE_LIMIT_REQUESTS", 120),
 		RateLimitWindow:   envInt("LOGMESH_RATE_LIMIT_WINDOW_SECONDS", 60),
+		JWTSecret:         envString("LOGMESH_JWT_SECRET", "change-me-in-production"),
+		KafkaBrokers:      envString("LOGMESH_KAFKA_BROKERS", ""),
+		KafkaLogsTopic:    envString("LOGMESH_KAFKA_LOGS_TOPIC", "logs"),
+		KafkaDLQTopic:     envString("LOGMESH_KAFKA_DLQ_TOPIC", "logs-dlq"),
+		OpenSearchURL:     envString("LOGMESH_OPENSEARCH_URL", ""),
+		PostgresURL:       envString("LOGMESH_POSTGRES_URL", ""),
+		RedisURL:          envString("LOGMESH_REDIS_URL", ""),
+		ProcessorWorkers:  envInt("LOGMESH_PROCESSOR_WORKERS", 4),
+		ProcessorBatchSize: envInt("LOGMESH_PROCESSOR_BATCH_SIZE", 500),
+		ProcessorBatchMS:  envInt("LOGMESH_PROCESSOR_BATCH_MS", 1000),
 	}
 }
 
